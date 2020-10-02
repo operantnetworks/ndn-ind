6c7714b8ec0664d88ffb0ca9aaf10d464ce43b30
jefft0 <>
2020-10-02 07:58:05 -0700

    encrypt (minor): In DecryptorV2::decrypt(Interest), fix error message. See pull request #11.

--- src/encrypt/decryptor-v2.cpp
+++ src/encrypt/decryptor-v2.cpp
@@ -58,7 +58,7 @@ DecryptorV2::decrypt
     encryptedContent->wireDecodeV2(interest.getApplicationParameters());
   } catch (const std::exception& ex) {
     onError(EncryptError::ErrorCode::DecryptionFailure,
-      std::string("Error decoding the Data content as EncryptedContent: ") + ex.what());
+      std::string("Error decoding the Interest ApplicationParameters as EncryptedContent: ") + ex.what());
     return;
   }
 
