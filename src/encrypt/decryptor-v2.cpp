bc4ddd80051bfc0569d11c0bcb12821c7c080085
jefft0 <>
2020-10-02 08:01:15 -0700

    encrypt (internal): In DecryptorV2::fetchGck onData, should set pendingInterest = 0 right away. See pull request #11.

--- src/encrypt/decryptor-v2.cpp
+++ src/encrypt/decryptor-v2.cpp
@@ -461,13 +461,13 @@ DecryptorV2::Impl::fetchGck
       (const ptr_lib::shared_ptr<const Interest>& ckInterest,
        const ptr_lib::shared_ptr<Data>& ckData)
     {
+      contentKey_->pendingInterest = 0;
+
       // Validate the Data signature.
       parent_->validator_->validate
         (*ckData,
         [=](auto&) {
           try {
-            contentKey_->pendingInterest = 0;
-
             // Pass an empty kdkKeyName so that we decrypt with the credentialsKey_.
             parent_->decryptCkAndProcessPendingDecrypts
               (*contentKey_, *ckData, Name(), onError_);
